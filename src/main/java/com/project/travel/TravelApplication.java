703bfa73a7940bbf445b3e62912bfc9abd2674dc
softwarerbfl <32202018@dankook.ac.kr>
2022-08-08 19:15:39 +0900

    서버 구축(1 warnings)

--- src/main/java/com/project/travel/TravelApplication.java
+++ src/main/java/com/project/travel/TravelApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class TravelApplication {
-
+    static {
+        System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+    }
     public static final String APPLICATION_LOCATIONS = "spring.config.location="
             + "classpath:application.properties,"
             + "classpath:aws.yml";
